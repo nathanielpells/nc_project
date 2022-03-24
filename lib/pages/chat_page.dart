45132dc79e153ef182039dc4853ed7d3e6d8a780
reece-b4 <reecebalfour@gmail.com>
2022-03-24 09:37:55 +0000

    begin firebase db connection

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
+
 
 class ChatPage extends StatefulWidget {
   @override
@@ -141,6 +143,7 @@ class _ChatPageState extends State<ChatPage> {
       '12:45',
     ),
   ];
+  List extendedChat = [];
 
   final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
 
@@ -154,50 +157,52 @@ class _ChatPageState extends State<ChatPage> {
         child: Column(children: [
           Flexible(
             child: SizedBox(
-              child: ListView.builder(
-                itemCount: _chat.length,
-                itemBuilder: (context, int index) => Card(
-                  child: ListTile(
-                    title: Align(
-                      alignment: _chat[index].username == 'User1'
-                          ? Alignment.centerRight
-                          : Alignment.centerLeft,
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: _chat[index].username == 'User1'
-                              ? Colors.blue
-                              : Colors.red,
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(12),
-                          child: Text(
-                            _chat[index].message,
-                            style: const TextStyle(
-                              color: Colors.white,
+              child: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection('channels')
+                      .snapshots(),
+                  builder: (context, AsyncSnapshot snapshot) {
+                    if (!snapshot.hasData) return const Text('Loading...');
+                    return ListView.builder(
+                      itemCount: snapshot.data.docs.User1_User2.thread.length,
+                      itemBuilder: (context, int index) => Card(
+                        child: ListTile(
+                          title: Align(
+                            alignment: _chat[index].username == 'User1'
+                                ? Alignment.centerRight
+                                : Alignment.centerLeft,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                color: _chat[index].username == 'User1'
+                                    ? Colors.blue
+                                    : Colors.red,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Text(
+                                  snapshot.data.docs[index].User1_User2
+                                      .thread[index].content,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                ),
-              ),
+                    );
+                  }),
             ),
           ),
-          const MessageInput(),
+          messageInput(),
         ]),
       ),
     );
   }
-}
 
-//set state or invoke db patch?
-class MessageInput extends StatelessWidget {
-  const MessageInput({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
+  Widget messageInput() {
     return Row(
       children: [
         Expanded(
@@ -206,10 +211,39 @@ class MessageInput extends StatelessWidget {
             decoration: const InputDecoration(
               hintText: 'type your message',
             ),
+            validator: (_value) => _value!.isNotEmpty ? null : '',
+            onChanged: (_value) {
+              setState(() {
+                _message = _value;
+                print(_value);
+              });
+            },
           ),
         ),
         ElevatedButton(
-          onPressed: () => print('message sent'),
+          onPressed: () => {
+            print(extendedChat.length),
+            setState(() {
+              extendedChat = [
+                ..._chat,
+                Chat(
+                  'https://i.pravatar.cc/50?img=50',
+                  'User1',
+                  '$_message',
+                  '12:45',
+                )
+              ];
+            }),
+            _chat.add(
+              Chat(
+                'https://i.pravatar.cc/50?img=50',
+                'User1',
+                '$_message',
+                '12:45',
+              ),
+            ),
+            print(extendedChat.length)
+          },
           child: const Text('Send'),
         ),
       ],
@@ -217,23 +251,35 @@ class MessageInput extends StatelessWidget {
   }
 }
 
+//difference between class and creating widget in register page.dart?
+//set state or invoke db patch?
+
+  //validation correct/needed/alternative needed?
+  //setstate error
+
+
+  //opens chat page from allmesages or send message from other users profile page
+  // call to db is made to check if chat already exists for 2 users
+  //if so their document? is retreived and messages data populates screen
+  //if not new collection? is made for both users
+  //current user field added/ variable state set?
+
+  //design and create firebase messages collection
+  //link from  screenshot/video
+  //authentication?
+  //patch to db function
+  //apply to ios and web
+
   //display avatars
   //onclick timestamp
   //add message input box and button ------- first
+  //remove card lines and improve styling
   //wrapping in messages
   //implement isuser true/false for alignment
   // data from db format changed to have isuser key?
   //refacter: extract some widgets to be outside of tree?
   //validation excludes empty messages
   // title uses non hardcoded udernames and avatars?
-  //scrolling functionality fully working?
   // drawer for other chats? 
-
-  // Widget _conversation(message) => Card(
-  //         child: Column(children: [
-  //       Text(message.username),
-  //       // Image(Image.network(message.avatar)),
-  //       // Text(message.message),
-  //       // Text(message.timestamp)
-  //     ]));
+  //auto scroll
 
