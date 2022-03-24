5d7ba2cd05d931f061882b8401f5e6b310d12f76
nathanielpells <93220158+nathanielpells@users.noreply.github.com>
2022-03-24 10:53:47 +0000

    add new page for camera functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:nc_project/pages/add_pet_page.dart';
+import 'package:nc_project/pages/camera_page.dart';
 
 // import 'package:nc_project/pages/home_page.dart';
 // import 'package:nc_project/pages/loading_page.dart';
 import 'package:nc_project/pages/login_page.dart';
 import 'package:nc_project/pages/profile_page.dart';
 import 'package:nc_project/pages/register_page.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(const MyApp(firstCamera));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,6 +23,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: TakePictureScreen(
+        camera: firstCamera,
+      ),
       title: 'Partial Pets',
       theme: ThemeData(
         primarySwatch: Colors.purple,
